0c8a44011a252fa9c3b87cef39e6828aea333b57
CaiocrNyimi <caiocrnyimi@gmail.com>
2025-11-06 12:54:00 -0300

    porta 80

--- MottuApi.API/Program.cs
+++ MottuApi.API/Program.cs
@@ -17,6 +17,11 @@ using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 var MyAllowAnyOriginPolicy = "_myAllowAnyOriginPolicy";
 
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(80);
+});
+
 // Rotas com nome minúsculo
 builder.Services.AddRouting(options =>
 {
